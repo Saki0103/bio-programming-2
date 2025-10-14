af8f5c398afff15f2016acab48d5ba036ce759b9
Saki0103 <sakikohyama@keio.jp>
2025-10-14 01:32:06 +0000

    .

---
+++ lec_3/example3-3.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main(void){
+    double x = 0;
+    double y = -0.1;
+
+    double allowable_error = 1e-12;
+    double error = 1;
+    int count = 0;
+    while(error>allowable_error){
+        double x_n = x - (exp(x) + x - 5)/(exp(x) + 1);
+        if(x_n>x){
+            error = x_n - x;
+        }else{
+            error = x - x_n;
+        }
+        x = x_n;
+        count++;
+        if(error<allowable_error){
+            cout << "x = " << x << endl;
+            cout << "count = " << count << endl;
+        }
+    }
+
+    while(error>allowable_error){
+        double y_n = y - 3*y;
+        if(y_n>x){
+            error = y_n - y;
+        }else{
+            error = y - y_n;
+        }
+        y = y_n;
+        count++;
+        if(error<allowable_error){
+            cout << "y = " << y << endl;
+            cout << "count = " << count << endl;
+        }
+    }
+
+    return 0;
+
+}
