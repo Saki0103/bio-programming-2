0a6f7af20f945b4f14984bedc807b33282f03a3b
Saki0103 <sakikohyama@keio.jp>
2025-12-23 20:00:39 +0000

    .

--- lec_11/kadai11-2.cpp
+++ lec_11/kadai11-2.cpp
@@ -39,18 +39,65 @@ int main(void){
     vector<vector<double>> expression_matrix(NUM_GENES, vector<double>(NUM_TISSUES, 0.0));
     LoadExpressionFile("gene_expression.txt", tissue_names, gene_names, expression_matrix);
 
-    vector<double> expression_max(NUM_GENES, 0.0);
+    vector<double> expression_average(NUM_GENES, 0.0);
+    for(int i = 0; i < NUM_GENES; i++){
+        for(int j = 0; j < NUM_TISSUES; j++){
+            expression_average[i] += expression_matrix[i][j];
+        }
+        expression_average[i] /= NUM_TISSUES;
+    }
 
-    double temp;
+    vector<double> expression_max(NUM_GENES, 0.0);
+    vector<int> expression_max_tissue(NUM_GENES, 0);
+    for(int i = 0; i < NUM_GENES; i++){
+        double max_value = expression_matrix[i][0];
+        for(int j = 1; j < NUM_TISSUES; j++){
+            if(expression_matrix[i][j] > max_value){
+                max_value = expression_matrix[i][j];
+                expression_max_tissue[i] = j;
+            }
+        }
+        expression_max[i] = max_value;
+    }
+    
+    cout << "組織特異的遺伝子数: " << endl;
+    vector<double> tau(NUM_GENES, 0.0);
+    vector<int> specific_genes(NUM_TISSUES, 0);
+    vector<int> ordered_specific_genes(NUM_TISSUES, 0);
+    for(int i = 0; i < NUM_GENES; i++){
+        double sum = 0.0;
+        for(int j = 0; j < NUM_TISSUES; j++){
+            sum += (1.0 - (expression_matrix[i][j] / expression_max[i]));
+        }
+        tau[i] = sum / (NUM_TISSUES - 1);
+        if(tau[i] > 0.85){
+            specific_genes[expression_max_tissue[i]]++;
+        }
+    }
+    
+    ordered_specific_genes = specific_genes;
     for(int i = 0; i < NUM_TISSUES; i++){
-        for(int j = i; j < NUM_TISSUES; j++){
-            if(expression_max[j] > expression_max[i]){
-                temp = expression_max[i];
-                expression_max[i] = expression_matrix[i][j];
+        for(int j = i + 1; j < NUM_TISSUES; j++){
+            if(ordered_specific_genes[i] < ordered_specific_genes[j]){
+                swap(ordered_specific_genes[i], ordered_specific_genes[j]);
+            }
+        }
+    }
+    for(int i = 0; i < NUM_TISSUES; i++){
+        for(int j = 0; j < NUM_TISSUES; j++){
+            if(specific_genes[j] == ordered_specific_genes[i]){
+                cout << tissue_names[j] << ": " << specific_genes[j] << endl;
+                if(i==0){
+                    for(int k = 0; k < NUM_GENES; k++){
+                        if(tau[k] >=0.95 && expression_max_tissue[k] == j && expression_average[k] >= 30.0){
+                            cout << "　特異性の高い遺伝子：" << gene_names[k] << endl;
+                        }
+                    }
+                }
+                
             }
         }
     }
     
-
     return 0;
 }
