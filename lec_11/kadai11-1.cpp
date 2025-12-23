9ccbe8eb2f9ab609f0b45504ce4d31494399148b
Saki0103 <sakikohyama@keio.jp>
2025-12-23 14:43:39 +0000

    .

---
+++ lec_11/kadai11-1.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+
+#define NUM_TISSUES 68
+#define NUM_GENES 19594
+
+using namespace std;
+
+void LoadExpressionFile(string file_name, vector<string>& tissue_name, 
+    vector<string>& gene_name, vector<vector<double>>& expression_matrix){
+
+    ifstream ifs(file_name);
+    if(!ifs){
+        cerr << "Error: Could not open file " << file_name << endl;
+        exit(1);
+    }
+    string tmp;
+    ifs >> tmp; 
+
+    for(int i = 0; i < NUM_TISSUES; i++){
+        ifs >> tissue_name[i];
+    }
+
+    for(int i = 0; i < NUM_GENES; i++){
+        ifs >> gene_name[i];
+        for(int j = 0; j < NUM_TISSUES; j++){
+            ifs >> expression_matrix[i][j];
+        }
+    }
+    ifs.close();
+}
+
+int main(void){
+    vector<string> tissue_names(NUM_TISSUES, "");
+    vector<string> gene_names(NUM_GENES, "");
+    vector<vector<double>> expression_matrix(NUM_GENES, vector<double>(NUM_TISSUES, 0.0));
+    LoadExpressionFile("gene_expression.txt", tissue_names, gene_names, expression_matrix);
+
+    vector<double> expression_average(NUM_GENES, 0.0);
+
+    for(int i = 0; i < NUM_GENES; i++){
+        for(int j = 0; j < NUM_TISSUES; j++){
+            expression_average[i] += expression_matrix[i][j];
+        }
+        expression_average[i] /= NUM_TISSUES;
+    }
+}
